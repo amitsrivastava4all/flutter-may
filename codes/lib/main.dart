c067fd70ebc125462e8ea49ac65aa5e479beaad7
Amit Srivastava <amit.srivastava4all@gmail.com>
2020-05-19 09:31:09 +0530

    Add files via upload

---
+++ codes/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import './screens/second.dart';
+
+void main() {
+  runApp(MaterialApp(title: 'My First App', home: Second()
+      //home: First(),
+      ));
+}
