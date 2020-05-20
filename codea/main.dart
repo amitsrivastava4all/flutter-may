4891e22df5b4b4c6cf9ed8df95620e5a7349c1e2
Amit Srivastava <amit.srivastava4all@gmail.com>
2020-05-20 08:53:02 +0530

    Add files via upload

---
+++ codea/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import './screens/greet.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'My First App',
+      //home: Second()
+      home: GreetApp()
+      //home: First(),
+      ));
+}
---
+++ codea/utils/stringoperations.dart
@@ -0,0 +1,5 @@
+class StringOperations {
+  static String initCap(String str) {
+    return str[0].toUpperCase() + str.substring(1).toLowerCase();
+  }
+}
